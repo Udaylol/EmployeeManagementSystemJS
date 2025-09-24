ae0c96b27c62c022b8788324ae9860a22cea12de
Uday <udaydhir794@gmail.com>
2025-09-24 12:24:48 +0530

    minor changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bee_project';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/managely';
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
